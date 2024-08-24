0315d4e21e8d322cb696332e97dd9099ac2db37c
karen <dani.k.c.b37@gmail.com>
2024-08-23 21:01:23 -0500

    scrip python 1

---
+++ scrips_python/ejemplo1.py
@@ -0,0 +1,14 @@
+#este es un comentario de linea
+'''
+esto es un comentario de bloque
+'''
+data ='''
+esto es una variable string dn bloque
+'''
+n1=10
+n2=20
+suma =n1+n2
+print(suma)
+print ("la suma es :" , suma)
+print (f"la suma es :  {suma}")       
+print ("la suma es :" , n1+n2)
