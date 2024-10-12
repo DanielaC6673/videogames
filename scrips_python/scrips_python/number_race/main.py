65f4f4f83ca975e1c9033122fb77d1bc970654fd
Karen Casanova <dani.k.c.b37@gmail.com>
2024-10-11 21:39:56 -0500

    actualizacio

---
+++ scrips_python/scrips_python/number_race/main.py
@@ -0,0 +1,97 @@
+'''
+Scripts description numbre race
+dev: Karen Casanova 
+date 13/08/2024
+'''
+
+from random import randint
+import os 
+
+status_menu = True
+
+def main_menu():
+    global status_opts
+    status_opts = True
+    print(":::main menu :::")
+    print ("[1]. Star Game")
+    print ("[2]. help")
+    print ("[3]. Exit")
+    
+    while status_opts:
+        opt = int(input("Press any option:"))
+        if opt < 1 or opt > 3:
+            print("Error. Press any option between 1 and 3")
+        else:
+            status_opts = False    
+        
+        return opt 
+
+while status_menu:
+    os.system('clear')   
+    op = main_menu()
+    if op == 1:
+        os.system('clear')
+        print(":::welcome to number race:::")
+        
+        players = int(input("Press number of players [1:4]"))
+         
+        print(":::level menu:::")
+        print ("[1].basic")
+        print ("[2].Intermediate")
+        print ("[3].Advance")
+        print ("[4].Expert" )
+        opt = int (input("press any option:"))
+        
+        if opt ==1:
+            pos = 20
+        elif opt == 2:
+            pos = 30
+        elif opt == 3:
+            pos = 50
+        else:
+            pos = 100
+            
+        #Start game 
+        status_game = True
+        roll_count = 0
+        roll_acum = 0
+        while status_game:
+            os.system('clear')
+            key = input("Press ani key to roll dice...")
+            
+            dice1 = randint(1,6)
+            dice2 = randint(1,6)
+
+            print(f"Dice 1:{dice1}")
+            print(f"Dice 2:{dice2}")
+            total = dice1 + dice2
+            print(f"Total roll: {total}")
+            print(f"Total game: {roll_acum}")
+            
+            roll_count += 1 #roll_count = roll_count + 1 
+            roll_acum += total # roll_acum = roll_acum + total
+            print(f"Total game: {roll_acum}")
+            
+            if roll_acum >= pos:
+                print("::: YOU WIN, CONGRATULATIONS :::")
+                status_game = False
+            
+            os.system('pause')
+                
+        print(":::STATITICS:::")
+        print(f"Total rolls: {roll_count}")
+        print(f"Total dices: {roll_acum}")    
+        
+        
+        key = input ("Press any key to go the main menu...")
+    elif op == 2:
+        print("help under construction")
+        key = input ("Press any key to go the main menu...")
+    else:
+        print("see 'u later")
+        key = input ("Press any key to exit...")
+        break
+        
+'''
+
+'''
